45b445fe18df9e738abdc27ea6657fc22e9e68ff
Dewmo123 <akh200808@gmail.com>
2024-10-22 00:00:35 +0900

    .complete add view

--- Assets/Work/JYE/Scripts/Enemy/MoveEnemy.cs
+++ Assets/Work/JYE/Scripts/Enemy/MoveEnemy.cs
@@ -7,6 +7,8 @@ public class MoveEnemy : Enemy
     protected override void Update()
     {
         base.Update();
+        movePoint1.position = new Vector3(movePoint1.position.x, transform.position.y);
+        movePoint2.position = new Vector3(movePoint2.position.x, transform.position.y);
     }
 
     public override void EnemyDie()
