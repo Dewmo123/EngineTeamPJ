8fa6154fadffb20b5307deb48f62f28f8ff21ab0
Dewmo123 <akh200808@gmail.com>
2024-11-18 00:49:42 +0900

    .fix stagechoose

--- Assets/Work/AKH/Scripts/UI/StartScene.cs
+++ Assets/Work/AKH/Scripts/UI/StartScene.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class StartScene : MonoBehaviour
 {
+    private Dictionary<string, MoveUIManager> _managers = new Dictionary<string, MoveUIManager>();
+    private void Awake()
+    {
+        GetComponentsInChildren<MoveUIManager>().ToList().ForEach(item => _managers.Add(item.gameObject.name, item));
+        _managers["StartSceneUI"].MoveTargetPos();
+    }
     public void StartGame()
     {
+        _managers["ChooseSceneUI"].MoveTargetPos(true);
+        _managers["StartSceneUI"].MoveOriginPos(true);
         using (StreamWriter sw = new StreamWriter(File.Open("asd.txt", FileMode.OpenOrCreate)))
         {
             sw.Write("asd");
--- Assets/Work/LSW/Scripts/Stage/StageManager.cs
+++ Assets/Work/LSW/Scripts/Stage/StageManager.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
-    private static StageManager StageData { get; set; }
+    private static StageManager Instance { get; set; }
     [SerializeField] private GameObject[] stageInfo;
 
     private void Awake()
     {
-        if (StageData == null)
+        if (Instance == null)
         {
-            StageData = this;
+            Instance = this;
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -33,7 +33,7 @@ public class StageManager : MonoBehaviour
     {
         for (int i = 0; i < stageInfo.Length; i++)
         {
-            PlayerPrefs.SetInt($"Stage_Unlock{i}", stageInfo[i].GetComponent<Stage>()._isUnlock ? 1 : 0);
+            PlayerPrefs.SetInt($"Stage_Unlock{i}", stageInfo[i].GetComponent<Stage>()._isUnlock.Value ? 1 : 0);
         }
         PlayerPrefs.Save();
     }
@@ -42,7 +42,7 @@ public class StageManager : MonoBehaviour
     {
         for (int i = 0; i < stageInfo.Length; i++)
         {
-            stageInfo[i].GetComponent<Stage>()._isUnlock = PlayerPrefs.GetInt($"Stage_Unlock{i}") == 1;
+            stageInfo[i].GetComponent<Stage>()._isUnlock.Value = PlayerPrefs.GetInt($"Stage_Unlock{i}") == 1;
         }
     }
 
