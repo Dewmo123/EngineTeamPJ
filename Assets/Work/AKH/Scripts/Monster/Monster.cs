61adb4e405597fd83ce575eb992336a7c9144d16
Dewmo123 <akh200808@gmail.com>
2024-11-26 00:10:52 +0900

    .add Camera Move

--- Assets/Work/AKH/Scripts/Monster/Monster.cs
+++ Assets/Work/AKH/Scripts/Monster/Monster.cs
@@ -1,3 +1,5 @@
+using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +10,7 @@ public class Monster : MonoBehaviour
     private Animator _animator;
     private Rigidbody2D _rb;
     public UnityEvent WalkEvent;
+    [SerializeField] private CinemachineVirtualCamera _mainCam;
 
     [SerializeField] private float _speed;
     private bool _endTriggerCalled = false;
@@ -21,24 +24,29 @@ public class Monster : MonoBehaviour
         if (_endTriggerCalled)
         {
             _animator.SetBool("Show", false);
+            _animator.SetBool("Idle", true);
+            StartCoroutine(Chase());
         }
     }
-    private void FixedUpdate()
+    private IEnumerator Chase()
     {
-        if (_endTriggerCalled)
-        {
-            _rb.velocity = new Vector2(_speed, _rb.velocity.y);
-        }
+        _endTriggerCalled = false;
+        _mainCam.Priority = 11;
+        yield return new WaitForSeconds(1.5f);
+        _animator.SetBool("Idle", false);
+        _rb.velocity = new Vector2(_speed, _rb.velocity.y);
     }
+
     public void Flip()
     {
         _speed = -_speed;
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y == 0 ? 180 : 0,0);
+        _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y == 0 ? 180 : 0, 0);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GamePlayer player;
-        if(collision.gameObject.TryGetComponent(out player))
+        if (collision.gameObject.TryGetComponent(out player))
         {
             player.SetDeadState();
             _rb.velocity = Vector2.zero;
